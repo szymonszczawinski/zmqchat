9fb1db879ad25e9f5a1a4cf9b641166b95152d67
Szymon Szczawński <szymonszczawinski@gmail.com>
2026-09-14 11:43:59 +0200

    add heartbeat
    - add heartbeat to client and server;
    - client - send ping every 2s when not active;
    - server - logout user on 6s w/o ping received

--- zmqchatserver/internal/server/session.go
+++ zmqchatserver/internal/server/session.go
@@ -7,17 +7,19 @@ import (
 )
 
 // ============================================================================
-// 2. SESSION MANAGER (Zarządzanie sesjami użytkowników)
+// 2. SESSION MANAGER (user sessions management)
 // ============================================================================
 
 type SessionManager struct {
-	mu    sync.RWMutex
-	users map[string]string // sessionToken -> username
+	mu       sync.RWMutex
+	users    map[string]string    // sessionToken -> username
+	lastSeen map[string]time.Time // username -> timestamp
 }
 
 func NewSessionManager() *SessionManager {
 	return &SessionManager{
-		users: make(map[string]string),
+		users:    make(map[string]string),
+		lastSeen: make(map[string]time.Time),
 	}
 }
 
@@ -28,4 +30,14 @@ func (sm *SessionManager) CreateSession(username string) string {
 	token := fmt.Sprintf("token-%s-%d", username, time.Now().Unix())
 	sm.users[token] = username
 	return token
+}
+
+// TouchUser update last user activity
+func (sm *SessionManager) TouchUser(username string) {
+	if username == "" {
+		return
+	}
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.lastSeen[username] = time.Now()
 }
